a366c0f772f37065de0ec5b7b295760b03f3f928
mtmcduffie <mcduffie.michael@gmail.com>
2017-06-22 09:56:23 -0400

    Adding run command.

---
+++ deploy/run_ecs.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+aws ecs run-task --cluster MD5S3-PROD \
+                 --task-definition MD5S3-PROD-task_family \
+                 --profile 64 \
+                --overrides "{\"containerOverrides\":[{\"name\":\"MD5S3-PROD\",\"environment\": [{\"name\":\"INPUT_QUEUE_NAME\",\"value\": \"MD5\"},{\"name\":\"OUTPUT_QUEUE_NAME\",\"value\": \"MD5_DONE\"}]}]}"
