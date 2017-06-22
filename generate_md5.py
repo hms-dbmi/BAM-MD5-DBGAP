004b71357275c6520508e3eda5c3d430913d7e1a
mtmcduffie <mcduffie.michael@gmail.com>
2017-06-22 09:19:44 -0400

    Updating.

--- generate_md5.py
+++ generate_md5.py
@@ -1,7 +1,6 @@
 import boto3
 import hashlib
 import os
-import copy
 import subprocess
 import errno
 import sys
@@ -15,39 +14,23 @@ def silentremove(filename):
         if e.errno != errno.ENOENT:
             raise
 
+input_queue_name = os.environ["INPUT_QUEUE_NAME"]
+output_queue_name = os.environ["OUTPUT_QUEUE_NAME"]
 
 # --------------------------
 # RESOURCES
 # --------------------------
 s3_resource = boto3.resource("s3")
 s3_client = boto3.client('s3')
+sqs = boto3.resource('sqs')
+
+print("INPUT_QUEUE_NAME " + input_queue_name)
+print("OUTPUT_QUEUE_NAME " + output_queue_name)
+
+input_queue = sqs.get_queue_by_name(QueueName=input_queue_name)
+output_queue = sqs.get_queue_by_name(QueueName=output_queue_name)
 # --------------------------
 
-# --------------------------
-# PARSING ID FILE
-# --------------------------
-ID_FILE = "/output/s3_fileservice"
-SAMPLE_ID_FILE = "/output/sample_fileservice"
-
-file_keys = []
-
-with open(ID_FILE, "r") as id_file_handle:
-    for entry in id_file_handle:
-        tmp = str(entry).strip().split(",")
-        try:
-            file_keys.append([tmp[0], tmp[1]])
-        except: pass
-
-file_keys_copy = copy.deepcopy(file_keys)
-
-file_sample_map = {}
-
-with open(SAMPLE_ID_FILE, "r") as id_file_handle:
-    for entry in id_file_handle:
-        tmp = str(entry).strip().split(",")
-        try:
-            file_sample_map[tmp[0]] = [tmp[1], tmp[2]]
-        except: pass
 
 # --------------------------
 
@@ -56,72 +39,103 @@ tempDownloadedFile = "/scratch/md5"
 
 # Set up output file.
 timestr = time.strftime("%Y%m%d-%H%M%S")
-md5_out_file = open("/scratch/md5_out" + timestr, "w+")
+md5_out_file = open("/scratch/md5_out" + timestr, "w+", 1)
 
-# --------------------------
-# LOOP THROUGH FILES TO PROCESS
-# --------------------------
-for file_key in file_keys:
+while True:
 
     # --------------------------
-    # DOWNLOAD FILES
+    # LOOP THROUGH MESSAGES TO PROCESS
     # --------------------------
-    file_s3_url = file_key[1]
-    fileservice_uuid = file_key[0]
-    bucket, key = file_s3_url.split('/', 2)[-1].split('/', 1)
+    print("Retrieving messages from queue - '" + input_queue_name + "'", flush=True)
 
-    print("Downloading File " + key)
-    s3_client.download_file(bucket, key, tempDownloadedFile)
+    for message in input_queue.receive_messages(MaxNumberOfMessages=1,
+                                          MessageAttributeNames=['UDN_ID', 'FileBucket', 'FileKey', 'sample_ID', 'file_service_uuid']):
 
-    # --------------------------
-    # PROCESS BAM FILES TO REPLACE IDS.
-    # --------------------------
+        print("Found Messages, processing.", flush=True)
 
-    UDN_ID = file_sample_map[fileservice_uuid][0]
-    Sample_ID = file_sample_map[fileservice_uuid][1]
+        if message.message_attributes is not None:
 
-    print("Processing BAM with samtools. UDN_ID - " + UDN_ID + " Sample_ID - " + Sample_ID, flush=True)
+            UDN_ID = message.message_attributes.get('UDN_ID').get('StringValue')
 
-    try:
-        subprocess.call(["/output/rehead_bam.sh", UDN_ID, Sample_ID])
-        print("Done processing file. Begin MD5.", flush=True)
-    except:
-        print("Error processing BAM - ", sys.exc_info()[:2], flush=True)
-        silentremove(tempDownloadedFile)
-        silentremove("/scratch/md5_reheader")
-        silentremove("/scratch/header.sam")
-        continue
+            FileBucket = message.message_attributes.get('FileBucket').get('StringValue')
+            FileKey = message.message_attributes.get('FileKey').get('StringValue')
 
-    # --------------------------
-    # GENERATE MD5
-    # --------------------------
-    md5 = hashlib.md5(open("/scratch/md5_reheader", 'rb').read()).hexdigest()
+            Sample_ID = message.message_attributes.get('sample_ID').get('StringValue')
+            fileservice_uuid = message.message_attributes.get('file_service_uuid').get('StringValue')
 
-    silentremove(tempDownloadedFile)
-    silentremove("/scratch/md5_reheader")
-    silentremove("/scratch/header.sam")
+            # --------------------------
+            # DOWNLOAD FILES
+            # --------------------------
+            print("Downloading File " + FileKey, flush=True)
+            s3_client.download_file(FileBucket, FileKey, tempDownloadedFile)
 
-    # --------------------------
-    # OUTPUT RESULTS
-    # --------------------------
-    md5_out_file.write("{0},{1},{2}\n".format(fileservice_uuid, file_key, md5))
+            # --------------------------
+            # PROCESS BAM FILES TO REPLACE IDS.
+            # --------------------------
 
-    print("----------\n")
-    print("FileService UUID : %s\n" % fileservice_uuid)
-    print("File Name : %s\n" % file_key)
-    print("MD5 : %s\n" % md5)
-    print("----------\n")
-    print("\n")
+            print("Processing BAM with samtools. UDN_ID - " + UDN_ID + " Sample_ID - " + Sample_ID, flush=True)
 
-    # --------------------------
-    # UPDATE FILES TO BE PROCESSED
-    # --------------------------
-    if len(file_keys_copy) > 0:
-        file_keys_copy.remove(file_key)
+            try:
+                subprocess.call(["/output/rehead_bam.sh", UDN_ID, Sample_ID])
+                print("Done processing file. Begin MD5.", flush=True)
+            except:
+                print("Error processing BAM - ", sys.exc_info()[:2], flush=True)
+                silentremove(tempDownloadedFile)
+                silentremove("/scratch/md5_reheader")
+                silentremove("/scratch/header.sam")
+                continue
 
-        with open(ID_FILE, 'w') as fp:
-            fp.truncate()
-            for x in file_keys_copy:
-                fp.write("{0},{1}\n".format(x[0], x[1]))
+            # --------------------------
+            # GENERATE MD5
+            # --------------------------
+            md5 = hashlib.md5(open("/scratch/md5_reheader", 'rb').read()).hexdigest()
 
-md5_out_file.close()
+            # --------------------------
+            # OUTPUT RESULTS
+            # --------------------------
+            md5_out_file.write("{0},{1},{2}\n".format(fileservice_uuid, FileKey, md5))
+
+            print("----------\n", flush=True)
+            print("FileService UUID : %s\n" % fileservice_uuid, flush=True)
+            print("File Name : %s\n" % FileKey, flush=True)
+            print("MD5 : %s\n" % md5, flush=True)
+            print("----------\n", flush=True)
+            print("\n", flush=True)
+
+            output_queue.send_message(MessageBody='boto3', MessageAttributes={
+                'UDN_ID': {
+                    'StringValue': UDN_ID,
+                    'DataType': 'String'
+                },
+                'FileBucket': {
+                    'StringValue': FileBucket,
+                    'DataType': 'String'
+                },
+                'FileKey': {
+                    'StringValue': FileKey,
+                    'DataType': 'String'
+                },
+                'sample_ID': {
+                    'StringValue': Sample_ID,
+                    'DataType': 'String'
+                },
+                'file_service_uuid': {
+                    'StringValue': fileservice_uuid,
+                    'DataType': 'String'
+                },
+                'md5': {
+                    'StringValue': md5,
+                    'DataType': 'String'
+                },
+                'file_type': {
+                    'StringValue': 'BAM',
+                    'DataType': 'String'
+                }
+            })
+
+            message.delete()
+            silentremove(tempDownloadedFile)
+            silentremove("/scratch/md5_reheader")
+            silentremove("/scratch/header.sam")
+
+    time.sleep(10)
